99e374e17cc78d0885cfbbb04a9e731b92a1de07
Gineth <ginysp@hotmail.com>
2021-04-13 04:15:56 +0000

    inclide

--- src/models.py
+++ src/models.py
@@ -29,5 +29,34 @@ class Address(Base):
     def to_dict(self):
         return {}
 
+class Follower(Base):
+    __tablename__ = 'follower'
+    user_from_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
+    user_to_id = Column(Integer, ForeignKey('user.id'),primary_key=True)
+    user = relationship(User)
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(50), nullable=False)
+    firstname = Column(String(150), nullable=False)
+    lastname = Column(String(150), nullable=False)
+    email = Column(String(250), nullable=True)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(550), nullable=False)
+    author_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey('post.id'), nullable=False)
+    post = relationship(Post)
+
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
